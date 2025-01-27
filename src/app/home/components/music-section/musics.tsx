1bd9b6fdb9feb951ae54fb65b62c788b85bf7426
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-01-27 09:54:32 -0300

    feat: created and refactored the music section

---
+++ src/app/home/components/music-section/musics.tsx
@@ -0,0 +1,15 @@
+import { MusicList } from "./components/music-list";
+
+const Musics = () => {
+  return (
+    <div className="flex flex-col px-4 py-5 gap-6">
+      <div className="flex items-center justify-between">
+        <p className="font-bold">Musics</p>
+        <p className="text-xs text-secondaryTextColor">View More</p>
+      </div>
+      <MusicList />
+    </div>
+  );
+};
+
+export default Musics;
