3246267333a2982f42babf07a695d9b44b56587a
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-01-27 09:55:30 -0300

    feat: custom hook to grab bands from backend

---
+++ src/hooks/useFetchBands.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+interface Band {
+  id: string;
+  name: string;
+  description: string;
+}
+
+export const useFetchBands = (userId: string | null) => {
+  const [bands, setBands] = useState<Band[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!userId) {
+      setError("No user ID provided");
+      setIsLoading(false);
+      return;
+    }
+
+    const fetchBands = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8080/bands/leader/${userId}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Error fetching user bands");
+        }
+
+        const data = await response.json();
+        setBands(data);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (err: any) {
+        setError(err.message || "An unexpected error occurred");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchBands();
+  }, [userId]);
+
+  return { bands, isLoading, error };
+};
