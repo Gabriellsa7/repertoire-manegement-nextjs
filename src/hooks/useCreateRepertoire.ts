9d25a3b2f107d3a199524af8ca196e01ceb9422a
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-02-04 07:27:17 -0300

    feat: created the hook to handle with the repertoire creation

---
+++ src/hooks/useCreateRepertoire.ts
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+interface Repertoire {
+  name: string;
+  imageUrl?: string;
+}
+
+interface UseCreateRepertoireResponse {
+  createRepertoire: (
+    repertoire: Repertoire
+  ) => Promise<{ success: boolean; error: string | null }>;
+  isLoading: boolean;
+  error: string | null;
+}
+
+export const useCreateRepertoire = (): UseCreateRepertoireResponse => {
+  const [isLoading] = useState(false);
+  const [error] = useState<string | null>(null);
+
+  const createRepertoire = async ({ name, imageUrl }: Repertoire) => {
+    const userId = localStorage.getItem("userId");
+
+    if (!userId) {
+      throw new Error("User ID is not found");
+    }
+
+    try {
+      const response = await fetch("http://localhost:8080/repertoire", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name,
+          imageUrl,
+        }),
+      });
+
+      if (!response.ok) {
+        const errorMessage =
+          (await response.text()) || "Failed to create the band.";
+        throw new Error(errorMessage);
+      }
+      return { success: true, error: null };
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      return {
+        success: false,
+        error: error.message || "An unknown error occurred.",
+      };
+    }
+  };
+
+  return { createRepertoire, isLoading, error };
+};
