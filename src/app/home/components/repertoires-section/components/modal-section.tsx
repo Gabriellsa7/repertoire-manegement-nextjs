5ed29cfb714af47ef2638deefb6d605d52b6fab7
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-02-10 07:59:48 -0300

    feat: created the logic ro create a repertoire

--- src/app/home/components/repertoires-section/components/modal-section.tsx
+++ src/app/home/components/repertoires-section/components/modal-section.tsx
@@ -1,6 +1,7 @@
 "use client";
+import { useCreateRepertoire } from "@/hooks/useCreateRepertoire";
 import { useState } from "react";
-import { Toaster } from "react-hot-toast";
+import { Toaster, toast } from "react-hot-toast";
 
 interface ModalSectionProps {
   isOpen: boolean;
@@ -8,8 +9,36 @@ interface ModalSectionProps {
 }
 
 export const ModalSection = ({ isOpen, onClose }: ModalSectionProps) => {
+  const { createRepertoire } = useCreateRepertoire();
   const [name, setName] = useState("");
   const [imageUrl, setImageUrl] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmitCreateRepertoire = async () => {
+    try {
+      const { success } = await createRepertoire({
+        name,
+        imageUrl,
+        description,
+      });
+
+      if (success) {
+        toast.success("Band created successfully!", {
+          position: "top-right",
+          duration: 1000,
+        });
+        setName(""); // Clear input field after successful creation
+        setImageUrl("");
+        onClose(); // Close modal on success
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Error creating band. Please try again.", {
+        position: "top-right",
+        duration: 1000,
+      });
+    }
+  };
 
   if (!isOpen) return null;
   return (
@@ -30,23 +59,34 @@ export const ModalSection = ({ isOpen, onClose }: ModalSectionProps) => {
             <label className="block mb-2 font-bold text-white">Name</label>
             <input
               type="text"
-              placeholder="Enter Band Name"
+              placeholder="Enter Repertoire Name"
               className="w-full p-3 outline-none rounded-lg mb-4 text-black"
               value={name}
               onChange={(e) => setName(e.target.value)}
+            />
+            <label className="block mb-2 font-bold text-white">
+              Description
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Repertoire Description"
+              className="w-full p-3 outline-none rounded-lg mb-4 text-black"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             />
             <label className="block mb-2 font-bold text-white">
               Repertoire Image Url
             </label>
             <input
               type="text"
-              placeholder="Enter Band Image"
+              placeholder="Enter Repertoire Image"
               className="w-full p-3 outline-none rounded-lg mb-4 text-black"
               value={imageUrl}
               onChange={(e) => setImageUrl(e.target.value)}
             />
 
             <button
+              onClick={handleSubmitCreateRepertoire}
               type="submit"
               className="bg-teal-500 text-white px-4 py-2 rounded-lg w-full"
             >
