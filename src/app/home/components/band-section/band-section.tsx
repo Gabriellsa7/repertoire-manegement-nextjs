f83d26d940e54a089a51c2d802bfc8de863d8255
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-02-19 11:21:16 -0300

    feat: added router to band-info and created a overflow-x

--- src/app/home/components/band-section/band-section.tsx
+++ src/app/home/components/band-section/band-section.tsx
@@ -3,12 +3,19 @@ import Image from "next/image";
 import { ModalSection } from "./components/modal-section";
 import { useFetchBands } from "@/hooks/useFetchBands";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const BandSection = () => {
   const userId =
     typeof window !== "undefined" ? localStorage.getItem("userId") : null;
   const { bands } = useFetchBands(userId);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const router = useRouter();
+
+  const handleSubmitBandInfo = () => {
+    router.push(`/band-info`);
+  };
 
   return (
     <div className="relative flex flex-col px-4 py-5 gap-6">
@@ -20,18 +27,20 @@ const BandSection = () => {
           <span className="text-secondaryTextColor text-xs">View More</span>
         </button>
       </div>
-      <div className="flex gap-3 items-center">
+      <div className="flex gap-3 items-center overflow-x-auto w-full whitespace-nowrap scrollbar-hide">
         {bands.length > 0
           ? bands.map((band) => (
-              <div key={band.id} className="flex flex-col gap-2">
-                <div className="flex justify-center items-center w-16 h-16 bg-gray-100 rounded-full overflow-hidden">
+              <div key={band.id} className="flex flex-col gap-2 min-w-[80px]">
+                <div className="flex justify-center items-center w-16 h-16 bg-gray-100 rounded-full">
                   {band.imageUrl && (
-                    <Image
-                      src={band.imageUrl}
-                      alt={band.name}
-                      width={70}
-                      height={70}
-                    />
+                    <button onClick={handleSubmitBandInfo}>
+                      <Image
+                        src={band.imageUrl}
+                        alt={band.name}
+                        width={70}
+                        height={70}
+                      />
+                    </button>
                   )}
                 </div>
                 <div className="w-20">
@@ -43,7 +52,7 @@ const BandSection = () => {
             ))
           : ""}
         <button
-          className="flex bg-transparent pb-9 items-center"
+          className="flex bg-transparent items-center min-w-[80px] pb-9"
           onClick={() => setIsModalOpen(true)}
         >
           <Image
