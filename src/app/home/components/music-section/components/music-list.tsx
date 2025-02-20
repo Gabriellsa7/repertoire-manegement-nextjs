46b9ce960a61344090986143bc3e2c4be3126814
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-02-20 12:02:17 -0300

    feat: added a router to music details

--- src/app/home/components/music-section/components/music-list.tsx
+++ src/app/home/components/music-section/components/music-list.tsx
@@ -2,9 +2,17 @@
 import React from "react";
 import Image from "next/image";
 import { useFetchMusics } from "@/hooks/useFetchMusics";
+import { useRouter } from "next/navigation";
 
 export const MusicList = () => {
   const { musics, loading, error } = useFetchMusics();
+
+  const router = useRouter();
+
+  const handleButton = () => {
+    router.push("/create-music");
+  };
+
   return (
     <div className="flex gap-5 flex-wrap">
       {loading ? (
@@ -13,7 +21,11 @@ export const MusicList = () => {
         <p className="text-red-500">{error}</p>
       ) : musics.length > 0 ? (
         musics.map((music) => (
-          <div className="flex flex-col gap-2" key={music.id}>
+          <div
+            className="flex flex-col gap-2"
+            key={music.id}
+            onClick={() => router.push(`/music/${music.id}`)}
+          >
             <div className="flex flex-col justify-center items-center w-16 h-16 bg-gray-100 rounded-xl overflow-hidden p-3"></div>
             <div className="w-16">
               <h3 className="text-lg font-bold truncate">{music.title}</h3>
@@ -25,7 +37,7 @@ export const MusicList = () => {
       )}
 
       {/* Add button */}
-      <button className="pb-9">
+      <button className="pb-9" onClick={handleButton}>
         <Image src="/assets/plus2.png" alt="plus icon" width={32} height={32} />
       </button>
     </div>
