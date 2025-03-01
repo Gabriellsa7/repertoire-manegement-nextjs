348a5ce798e7a99950b1e85dc4b33ed5b526182b
Gabriellsa7 <gabrielsantana123santos@gmail.com>
2025-03-01 10:49:36 -0300

    feat: changed a name

--- src/app/home/components/music-section/musics.tsx
+++ src/app/home/components/music-section/musics.tsx
@@ -4,7 +4,7 @@ const Musics = () => {
   return (
     <div className="flex flex-col px-4 py-5 gap-6">
       <div className="flex items-center justify-between">
-        <p className="font-bold">Musics</p>
+        <p className="font-bold">Music</p>
         <p className="text-xs text-secondaryTextColor">View More</p>
       </div>
       <MusicList />
