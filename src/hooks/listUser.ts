2f1d2cf1fc6be41573cae543f422229a6d18670b
Gabriellsa7 <gabrielsantana123santos@gmail.com>
2025-03-01 10:48:05 -0300

    feat: created the hook to list users

---
+++ src/hooks/listUser.ts
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+interface User {
+  id: string;
+  name: string;
+}
+
+export const useFetchUsers = (): {
+  users: User[];
+  isLoading: boolean;
+  error: string | null;
+} => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch("http://localhost:8080/user");
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+        const data = await response.json();
+        setUsers(data);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (err: any) {
+        setError(err.message || "An unknown error occurred.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return { users, isLoading, error };
+};
