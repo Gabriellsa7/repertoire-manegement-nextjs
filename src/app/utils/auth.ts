d8a19b94453c62ba6c42dbb056ad48d0b9ba3873
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2025-03-16 09:50:51 -0300

    feat: changed method name and added a new method

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -1,5 +1,10 @@
-export const getLoggedUsers = () => {
+export const getAllUsers = () => {
   return Object.keys(localStorage)
     .filter((key) => key.startsWith("user_"))
     .map((key) => JSON.parse(localStorage.getItem(key) || "{}"));
+};
+
+export const getLoggedUser = () => {
+  const loggedUser = localStorage.getItem("user_logged");
+  return loggedUser ? JSON.parse(loggedUser) : null;
 };
